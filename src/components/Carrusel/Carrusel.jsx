667125bf9aeebcde51e2eefc8a4298bb556ab7fa
Cesar Azpeitia <cesaralonso.azpeitia@gmail.com>
2023-06-08 22:35:44 -0600

    proyecto

--- src/components/Carrusel/Carrusel.jsx
+++ src/components/Carrusel/Carrusel.jsx
@@ -2,11 +2,11 @@ import React from 'react'
 import "./Carrusel.css"
 
 const Carrusel = () => {
-  return (
-    <div className="Carrusel">
-        Carrusel de textos
-    </div>
-  )
+    return (
+        <div className="Carrusel">
+            Carrusel de textos
+        </div>
+    )
 }
 
 export default Carrusel
